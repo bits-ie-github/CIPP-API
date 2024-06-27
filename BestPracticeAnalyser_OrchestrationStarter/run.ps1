f560d5589673e65668238a9cd81ddf518283253b
bits-ie-github <166813582+bits-ie-github@users.noreply.github.com>
2024-06-27 20:28:02 +0100

    Update run.ps1

--- BestPracticeAnalyser_OrchestrationStarter/run.ps1
+++ BestPracticeAnalyser_OrchestrationStarter/run.ps1
@@ -11,6 +11,7 @@ if ($Request.Query.TenantFilter) {
 
 $BPATemplateTable = Get-CippTable -tablename 'templates'
 $Filter = "PartitionKey eq 'BPATemplate'"
+$Filter = "GUID eq 'BITS - Best Practices v1 - Table view'"
 $Templates = ((Get-CIPPAzDataTableEntity @BPATemplateTable -Filter $Filter).JSON | ConvertFrom-Json).Name
 
 $BPAReports = foreach ($Tenant in $TenantList) {
